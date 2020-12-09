2f933907ccc7de67ffcfd87b695b0ca617094208
Joah Schissel <jtschiss@gmail.com>
2020-12-08 19:21:17 -0600

    got role dao working

---
+++ src/main/java/edu/matc/persistence/GenericDao.java
@@ -0,0 +1,2 @@
+package edu.matc.persistence;public class GenericDao {
+}
---
+++ src/test/java/edu/matc/persistence/UserRoleDaoTest.java
@@ -0,0 +1,27 @@
+package edu.matc.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserRoleDaoTest {
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void saveOrUpdate() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
