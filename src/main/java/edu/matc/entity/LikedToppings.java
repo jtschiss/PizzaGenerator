09865ca1a07a858934766bb8dd26a08cc7cc13f0
Joah Schissel <jtschiss@gmail.com>
2020-12-09 04:04:35 -0600

    added most of the dao functionality

--- src/main/java/edu/matc/entity/LikedToppings.java
+++ src/main/java/edu/matc/entity/LikedToppings.java
@@ -1,21 +1,2 @@
-package edu.matc.entity;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class LikedToppings {
-
-    private List<String> likedToppings = new ArrayList<String>();
-
-    public LikedToppings() {
-
-    }
-
-    public void setLikedToppings(List<String> likedToppings) {
-        this.likedToppings = likedToppings;
-    }
-
-    public List<String> getLikedToppings() {
-        return likedToppings;
-    }
+package edu.matc.entity;public class LikedToppings {
 }
