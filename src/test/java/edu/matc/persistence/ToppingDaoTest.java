b5541e9171f8cbf36be1eb6e7c37b36a8a358172
Joah Schissel <jtschiss@gmail.com>
2020-12-08 20:33:01 -0600

    added topping Dao and tests

---
+++ src/test/java/edu/matc/persistence/ToppingDaoTest.java
@@ -0,0 +1,11 @@
+package edu.matc.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ToppingDaoTest {
+
+    @Test
+    void getAll() {
+    }
+}
